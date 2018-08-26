b2d0e23ee6cdd9aa3c7241381646b68f15e05ed2
jose cervantes <abdn0518@gmail.com>
2018-08-25 21:59:38 -0500

    Added edit, delete feature and appropiate tests

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . models import Post
+
 
 # Create your views here.
 class BlogListView(ListView): # pylint: disable=too-many-ancestors
     model = Post
     template_name = 'home.html'
 
-class BlogDetailView(DetailView):
+class BlogDetailView(DetailView): # pylint: disable=too-many-ancestors
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView): # pylint: disable=too-many-ancestors
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView): # pylint: disable=too-many-ancestors
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+class BlogDeleteView(DeleteView): # pylint: disable=too-many-ancestors
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
